783404e916bbe25d3985e1145a325862a61a6432
Ryan Tuck <ryntck@gmail.com>
2020-07-08 23:06:18 -0500

    yes, backlinks

--- compile.py
+++ compile.py
@@ -1,10 +1,5 @@
 """
-Compile mdzk docs to hugo-friendly format.
-
-Get all files in specific dir by default.
-
-TODO
-- Generate backlinks
+Compile markdown notes to hugo-friendly format.
 """
 from dataclasses import dataclass
 import os
@@ -39,18 +34,23 @@ class Note:
         results = re.findall(tag_regex, self.body()) or []
         return results
 
+    def _tag_link(self, tag):
+        tag_clean = tag.replace('-', ' ')
+        return f"[{tag_clean}]" + '({{< relref ' + f'"{tag}.md"' + " >}} )"
+
     def is_private(self):
         return '#private' in self.body()
 
-    def compiled_body(self):
+    def backlinks_section(self, backlinks):
+        return '\n'.join(
+            ['## Backlinks\n'] + [f'- {self._tag_link(b)}' for b in backlinks]
+        )
+
+    def compiled_body(self, backlinks):
         body = self.body()
         for tag in self.tags():
-            tag_clean = tag.replace('-', ' ')
-            body = body.replace(
-                f'[[{tag}]]',
-                f"[{tag_clean}]" + '({{< relref ' + f'"{tag}.md"' + " >}} )",
-            )
-        return body
+            body = body.replace(f'[[{tag}]]', self._tag_link(tag))
+        return body + '\n\n' + self.backlinks_section(backlinks)
 
 
 def md_files():
@@ -62,13 +62,24 @@ def md_files():
 def compile_notes():
     shutil.rmtree(COMPILED_DIR)
     os.makedirs(COMPILED_DIR)
-    for filename in md_files():  # notes()
-        n = Note(filename=filename)
-        if n.is_private() or any(re.match(br, n.note_name()) for br in BLACKLIST_REGEX):
-            continue
+
+    all_notes = [Note(f) for f in md_files()]
+    valid_notes = [
+        n
+        for n in all_notes
+        if not (
+            n.is_private() or any(re.match(br, n.note_name()) for br in BLACKLIST_REGEX)
+        )
+    ]
+
+    # get all edges of graph
+    edges = [(n.note_name(), t) for n in valid_notes for t in n.tags()]
+
+    for n in valid_notes:
+        backlinks = [e[0] for e in edges if e[1] == n.note_name()]
         with open(f"{COMPILED_DIR}/{n.note_name()}.md", "w") as f:
             print(os.path.join(DOCS_DIR, n.filename))
-            f.write(n.compiled_body())
+            f.write(n.compiled_body(backlinks))
 
 
 def main():
